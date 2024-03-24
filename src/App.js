6a6a2ad4e74af56c1a53e4e5e8b5571828b69a84
Nguyễn Trúc An <114329768+nguyentrucan@users.noreply.github.com>
2024-03-24 22:17:38 +0700

    modified:   public/index.html 	modified:   src/App.css 	modified:   src/App.js 	modified:   src/components/Header.js

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Layout from './components/Layout';
 import Home from './pages/Home';
 import About from './pages/About';
 import Contact from './pages/Contact';
-function App() {
+const App = () => {
   return (
     <>
       <BrowserRouter>
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,24 @@
 import React from 'react'
+import { NavLink, Link } from 'react-router-dom'
 
-function Header() {
+const Header = () => {
   return (
-    <div>Header</div>
+    <>
+      <header className='header-top-strip py-3'>
+        <div className='container-xxl'>
+          <div className='row'>
+            <div className='col-6'>
+              <p className='text-white mb-0'>Free Shipping Over $100 & Free Returns</p>
+            </div>
+            <div className='col-6'>
+              <p className='text-end text-white mb-0'>
+                Hotline: <a className='text-white' href='tel:+84 399195099'>+84 399 195 099</a>
+              </p>
+            </div>
+          </div>
+        </div>
+      </header>
+    </>
   )
 }
 
