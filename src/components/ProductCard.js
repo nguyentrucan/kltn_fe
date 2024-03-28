4175addd4eb01e5f287ee7d9c55b696a29677db8
Nguyễn Trúc An <114329768+nguyentrucan@users.noreply.github.com>
2024-03-28 17:13:52 +0700

    modified:   package-lock.json 	modified:   package.json 	new file:   public/images/watch-01.jpg 	modified:   src/App.css 	new file:   src/components/ProductCard.js 	modified:   src/pages/Home.js

---
+++ src/components/ProductCard.js
@@ -0,0 +1,48 @@
+import React from 'react'
+import ReactStars from "react-rating-stars-component"
+import { Link } from 'react-router-dom'
+
+const ProductCard = () => {
+    return (
+        <div className='col-3'>
+            <Link className='product-card position-relative'>
+                <div className='wishlist-icon position-absolute'>
+                    <Link>
+                        <img src='images/wish.svg' alt='wishlist' />
+                    </Link>
+                </div>
+                <div className='product-image'>
+                    <img src='images/watch.jpg' className='img-fluid' alt='productimage' />
+                    <img src='images/watch-01.jpg' className='img-fluid' alt='productimage' />
+                </div>
+                <div className='product-details'>
+                    <h6 className='brand'>Havels</h6>
+                    <h5 className='product-title'>
+                        Kids headphones bulk 10 pack multi colored for students
+                    </h5>
+                    <ReactStars count={5} size={24} value='3' edit={false} activeColor="#ffd700"
+                    />
+                    <p className='price'>$100.00</p>
+                </div>
+                <div className='action-bar position-absolute'>
+                    <div className='d-flex flex-column gap-15'>
+                        <Link>
+                            <img src='images/add-cart.svg' alt='addcart' />
+                        </Link>
+                        <Link>
+                            <img src='images/prodcompare.svg' alt='compare' />
+                        </Link>
+                        <Link>
+                            <img src='images/view.svg' alt='view' />
+                        </Link>
+                        <Link>
+                            <img src='images/add-cart.svg' alt='addcart' />
+                        </Link>
+                    </div>
+                </div>
+            </Link>
+        </div>
+    )
+}
+
+export default ProductCard
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import Marquee from "react-fast-marquee";
 import BlogCard from '../components/BlogCard';
+import ProductCard from '../components/ProductCard';
 
 const Home = () => {
   return (
@@ -167,6 +168,19 @@ const Home = () => {
                 </div>
               </div>
             </div>
+          </div>
+        </div>
+      </section>
+      <section className='featured-wrapper py-5 home-wrapper-2'>
+        <div className='container-xxl'>
+          <div className='row'>
+            <div className='col-12'>
+              <h3 className='section-heading'>Featured Collection</h3>
+            </div>
+            <ProductCard />
+            <ProductCard />
+            <ProductCard />
+            <ProductCard />
           </div>
         </div>
       </section>
