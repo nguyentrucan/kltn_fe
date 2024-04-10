29e8aa1489b3fb747dfcb480e90aee17a3909e36
Nguyễn Trúc An <114329768+nguyentrucan@users.noreply.github.com>
2024-04-10 13:58:47 +0700

    modified:   src/App.css 	modified:   src/App.js 	new file:   src/components/Color.js 	modified:   src/components/Header.js 	new file:   src/pages/CompareProduct.js 	modified:   src/pages/OurStore.js

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import About from './pages/About';
 import Contact from './pages/Contact';
 import OurStore from './pages/OurStore';
 import Blog from './pages/Blog';
+import CompareProduct from './pages/CompareProduct';
 const App = () => {
   return (
     <>
@@ -18,6 +19,7 @@ const App = () => {
             <Route path='contact' element={<Contact />} />
             <Route path='store' element={<OurStore />} />
             <Route path='blogs' element={<Blog />} />
+            <Route path='compare-product' element={<CompareProduct />} />
           </Route>
         </Routes>
       </BrowserRouter>
--- src/pages/OurStore.js
+++ src/pages/OurStore.js
@@ -3,6 +3,7 @@ import BreadCrumb from '../components/BreadCrumb'
 import Meta from '../components/Meta'
 import ReactStars from "react-rating-stars-component"
 import ProductCard from '../components/ProductCard';
+import Color from '../components/Color';
 
 const OurStore = () => {
     const [grid, setGrid] = useState(4);
@@ -57,20 +58,7 @@ const OurStore = () => {
                                     </div>
                                     <h5 className='sub-title'>Colors</h5>
                                     <div>
-                                        <ul className='colors ps-0'>
-                                            <li></li>
-                                            <li></li>
-                                            <li></li>
-                                            <li></li>
-                                            <li></li>
-                                            <li></li>
-                                            <li></li>
-                                            <li></li>
-                                            <li></li>
-                                            <li></li>
-                                            <li></li>
-                                            <li></li>
-                                        </ul>
+                                        <Color/>
                                     </div>
                                     <h5 className='sub-title'>Size</h5>
                                     <div>
